715e29730c8d75b175ba38f1656042709163614d
Aurisfoxtro <94296988+Aurisfoxtro@users.noreply.github.com>
2023-10-04 15:28:06 +0300

    debuging

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
+import { Layout } from './components/Layout.js';
+import { Home } from './components/Home.js';
 
-//import './custom.css'
+import './custom.css'
 
 import {Pizzas} from './components/Pizza/Pizzas';
 import { Create } from './components/Pizza/Create';
@@ -15,8 +15,8 @@ export default class App extends Component {
   render () {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/pizzas' component={Pizzas} />
+        <Route exact path='/' component={Home}></Route>
+        <Route path='/pizzas' component={Pizzas}></Route>
         <Route path='/create' component = {Create}></Route>
       </Layout>
     );
